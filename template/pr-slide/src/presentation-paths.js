143ba5981736bebd9be867ea315c8ca84b03557c
mezig <wimpheling@gmail.com>
2026-05-25 17:46:55 +0100

    remove pr slide

--- template/pr-slide/src/presentation-paths.js
+++
@@ -1,9 +0,0 @@
-export function branchToPresentationDirName(branch) {
-  const slug = branch
-    .trim()
-    .replace(/^refs\/heads\//, "")
-    .replace(/[^a-zA-Z0-9._-]+/g, "-")
-    .replace(/^-+|-+$/g, "");
-
-  return slug || "detached-head";
-}
